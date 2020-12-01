61a78f83eff3f0586cdbed166102e6da8a01090f
Adrian.T <t.adrian88@gmail.com>
2020-12-01 20:32:48 +0000

    Added balance and matching green test.

--- lib/Bank.rb
+++ lib/Bank.rb
@@ -1,4 +1,11 @@
 class Bank
+
+    attr_reader :balance
+
+    def initialize
+        @balance = 0
+    end
+
     def deposit_money(money)
         1000
     end
--- spec/Bank_spec.rb
+++ spec/Bank_spec.rb
@@ -5,6 +5,10 @@ describe Bank do
         expect(Bank.new).to be_truthy()
     end
 
+    it "checks if the balance exist" do
+        expect(subject.balance).to be_truthy()
+    end
+
     it "customer can deposit money" do
         expect(subject.deposit_money(1000)).to eq 1000
     end
