3678475e2a5dd254f1e6feb6f8e33e2a9956ca98
Adrian.T <t.adrian88@gmail.com>
2020-12-01 20:36:59 +0000

    Added withdraw_money method with a green test.

--- lib/Bank.rb
+++ lib/Bank.rb
@@ -9,4 +9,8 @@ class Bank
     def deposit_money(money)
         1000
     end
+
+    def withdraw_money(money)
+        -500
+    end
 end
--- spec/Bank_spec.rb
+++ spec/Bank_spec.rb
@@ -9,7 +9,11 @@ describe Bank do
         expect(subject.balance).to be_truthy()
     end
 
-    it "customer can deposit money" do
+    it "checks if the customer can deposit money" do
         expect(subject.deposit_money(1000)).to eq 1000
+    end
+
+    it " checks if the customer can withdraw money" do
+        expect(subject.withdraw_money(-500)).to eq -500
     end
 end
